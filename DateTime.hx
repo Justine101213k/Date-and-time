94f6ac244bcae6481a3d8316e23b7200c7de7191
Cool GAMER <82664019+Justine101213k@users.noreply.github.com>
2022-01-18 18:09:17 +0800

    Add files via upload

---
+++ DateTime.hx
@@ -0,0 +1,81 @@
+package;
+
+import openfl.text.TextField;
+import openfl.text.TextFormat;
+import openfl.utils.Assets;
+import openfl.events.Event;
+
+// Made by Gtnv (Cool Gamer) Me
+class DateTime extends TextField
+{
+    private var days:String;
+    private var month:String;
+
+    public function new(x:Float = 10.0, y:Float = 10.0, colorString:String)
+    {
+        super();
+
+        this.x = x;
+        this.y = y;
+
+        selectable = false;
+        mouseEnabled = false;
+
+        defaultTextFormat = new TextFormat("_sans", 12, Std.parseInt(colorString));
+
+        addEventListener(Event.ENTER_FRAME, onDate);
+
+        width = 150;
+        height = 70;
+    }
+
+    private function onDate(none)
+    {
+        var ts:String;
+        var correctTime:String;
+
+        if (Date.now().getHours() < 10) hours = '0' + Std.string(Date.now().getHours()); else hours = Std.string(Date.now().getHours());
+    
+        if (Date.now().getMinutes() < 10) minutes = '0' + Std.string(Date.now().getMinutes()); else minutes = Std.string(Date.now().getMinutes());
+    
+        // copied from DateTools.hx lol
+        if (Date.now().getHours() > 11) ts = 'PM'; else ts = 'AM';
+
+        days = Calendar.DAYS_NAME[Date.now().getDay()];
+        month = Calendar.MONTH_NAME[Date.now().getMonth()];
+
+        if (visible) // If you toggle Date And Time options lol
+        {
+            // Swag
+            text = Std.string('\nTime: ' + hours + ':' + minutes + ' ' + ts + '\nDate: ' + month + ' ' + Date.now().getDate() + ', ' + Date.now().getFullYear() + '\nDay: ' + days);
+        }
+    }
+
+    var hours:String;
+    var minutes:String;
+}
+
+class Calendar
+{
+    public static var MONTH_SHORT_NAME = [
+        'Jan', 'Feb', 'Mar', 'Apr', 'May',
+        'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
+        'Nov', 'Dec'
+    ];
+
+    public static var MONTH_NAME = [
+        'January', 'February', 'March', 'April', 'May',
+        'June', 'July', 'August', 'September', 'October',
+        'November', 'December'
+    ];
+
+    public static var DAYS_SHORT_NAME = [
+        'Sun', 'Mon', 'Tue', 'Wed', 'Thu',
+        'Fri', 'Sat'
+    ];
+
+    public static var DAYS_NAME = [
+        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+        'Friday', 'Saturday'
+    ];
+}
