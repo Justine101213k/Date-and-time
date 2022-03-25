d28ef4201991024df2f1b6d3f054163d82bd1bfe
EnDev <82664019+Justine101213k@users.noreply.github.com>
2022-03-25 16:26:46 +0800

    removed Seconds Cuz it broke lol

--- DateTime.hx
+++ DateTime.hx
@@ -5,6 +5,8 @@ import openfl.text.TextFormat;
 import openfl.utils.Assets;
 import openfl.events.Event;
 
+using StringTools;
+
 // Made by Gtnv (Cool Gamer) Me
 class DateTime extends TextField
 {
@@ -22,7 +24,7 @@ class DateTime extends TextField
         selectable = false;
         mouseEnabled = false;
 
-        defaultTextFormat = new TextFormat("_sans", 12, Std.parseInt(colorString));
+        defaultTextFormat = new TextFormat(/*"_sans"*/Assets.getFont(Reference.getFont('alphabet')).fontName, 12, Std.parseInt(colorString));
 
         addEventListener(Event.ENTER_FRAME, onDate);
 
@@ -33,51 +35,40 @@ class DateTime extends TextField
     private function onDate(none)
     {
         var ts:String;
+        var e:Int = 10;
+        var now:Date = Date.now();
 
-        if (Date.now().getHours() < 10) hours = '0' + Std.string(Date.now().getHours()); else hours = Std.string(Date.now().getHours());
-    
-        if (Date.now().getMinutes() < 10) minutes = '0' + Std.string(Date.now().getMinutes()); else minutes = Std.string(Date.now().getMinutes());
+        if (now.getHours() <= e)
+            hours = '0' + Std.string(now.getHours());
+        else
+            hours = Std.string(now.getHours());
 
-        if (Date.now().getSeconds() <= 10) seconds = '0' + Std.string(Date.now().getMinutes()); else seconds = Std.string(Date.now().getSeconds());
-    
+        if (now.getMinutes() <= e)
+            minutes = '0' + Std.string(now.getMinutes());
+        else
+            minutes = Std.string(now.getMinutes());
+
+        /*if (now.getSeconds() <= e) // this broke ngl
+            seconds = '0' + Std.string(now.getSeconds());
+        else
+            seconds = Std.string(now.getMinutes());*/
+
         // copied from DateTools.hx lol
-        if (Date.now().getHours() > 11) ts = 'PM'; else ts = 'AM';
+        if (now.getHours() >= e + 1) ts = 'PM'; else ts = 'AM';
 
-        days = Calendar.DAYS_NAME[Date.now().getDay()];
-        month = Calendar.MONTH_NAME[Date.now().getMonth()];
+        // days = Calendar.DAYS_NAME[now.getDay()];
+        // month = Calendar.MONTH_NAME[now.getMonth()];
+
+        days = Calendar.days_name_map[now.getDay()]; // or Calendar.days_name_map.get(now.getDay());
+        month = Calendar.month_name_map[now.getMonth()]; // or Calendar.month_name_map.get(now.getMonth());
 
         if (visible)
         {
             // Swag
-            text = Std.string('\nTime: ' + hours + ':' + minutes + ':' + seconds + ' ' + ts + '\nDate: ' + month + ' ' + Date.now().getDate() + ', ' + Date.now().getFullYear() + '\nDay: ' + days);
+            text = Std.string('\nTime: ' + hours + ':' + minutes /*+ ':' + seconds*/ + ' ' + ts + '\nDate: ' + month + ' ' + now.getDate() + ', ' + now.getFullYear() + '\nDay: ' + days);
         }
     }
 
     var hours:String;
     var minutes:String;
-}
-
-class Calendar
-{
-    public static var MONTH_SHORT_NAME = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May',
-        'Jun', 'Jul', 'Aug', 'Sep', 'Oct',
-        'Nov', 'Dec'
-    ];
-
-    public static var MONTH_NAME = [
-        'January', 'February', 'March', 'April', 'May',
-        'June', 'July', 'August', 'September', 'October',
-        'November', 'December'
-    ];
-
-    public static var DAYS_SHORT_NAME = [
-        'Sun', 'Mon', 'Tue', 'Wed', 'Thu',
-        'Fri', 'Sat'
-    ];
-
-    public static var DAYS_NAME = [
-        'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
-        'Friday', 'Saturday'
-    ];
 }
