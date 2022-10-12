210d985a2a92b8435878c43f8713bd943f58ca9a
EnDev <82664019+Justine101213k@users.noreply.github.com>
2022-10-12 16:14:52 +0800

    Add files via upload

--- DateTime.hx
+++ DateTime.hx
@@ -2,73 +2,40 @@ package;
 
 import openfl.text.TextField;
 import openfl.text.TextFormat;
-import openfl.utils.Assets;
-import openfl.events.Event;
 
-using StringTools;
-
-// Made by Gtnv (Endev) Me
 class DateTime extends TextField
 {
-    private var days:String;
-    private var month:String;
-    private var seconds:String;
+        var minutes:String;
+        var hours:String;
+        var calendar:Calendar = Calendar.instance;
+        var curDate:Date = Date.now();
 
-    public function new(x:Float = 10.0, y:Float = 10.0, colorString:String)
-    {
-        super();
-
-        this.x = x;
-        this.y = y;
-
-        selectable = false;
-        mouseEnabled = false;
-
-        defaultTextFormat = new TextFormat("_sans", 12, Std.parseInt(colorString));
-
-        addEventListener(Event.ENTER_FRAME, onDate);
-
-        width = 150;
-        height = 70;
-    }
-
-    private function onDate(none)
-    {
-        var ts:String;
-        var e:Int = 10;
-        var now:Date = Date.now();
-
-        if (now.getHours() <= e)
-            hours = '0' + Std.string(now.getHours());
-        else
-            hours = Std.string(now.getHours());
-
-        if (now.getMinutes() <= e)
-            minutes = '0' + Std.string(now.getMinutes());
-        else
-            minutes = Std.string(now.getMinutes());
-
-        /*if (now.getSeconds() <= e) // this broke ngl
-            seconds = '0' + Std.string(now.getSeconds());
-        else
-            seconds = Std.string(now.getMinutes());*/
-
-        // copied from DateTools.hx lol
-        if (now.getHours() >= e + 1) ts = 'PM'; else ts = 'AM';
-
-        // days = Calendar.DAYS_NAME[now.getDay()];
-        // month = Calendar.MONTH_NAME[now.getMonth()];
-
-        days = Calendar.days_name_map.get(now.getDay());
-        month = Calendar.month_name_map.get(now.getMonth());
-
-        if (visible)
+        public override function new(x:Float = 0, y:Float = 0, color:Int = 0xFFFFFF)
         {
-            // Swag
-            text = Std.string('\nTime: ' + hours + ':' + minutes /*+ ':' + seconds*/ + ' ' + ts + '\nDate: ' + month + ' ' + now.getDate() + ', ' + now.getFullYear() + '\nDay: ' + days);
+                this.x = x;
+                this.y = y;
+                super();
+                mouseEnabled = false;
+                selectable = false;
+                defaultTextFormat = new TextFormat("_sans", 12, Std.parseInt(Std.string(color)));
         }
-    }
 
-    var hours:String;
-    var minutes:String;
+        private override function __update(transformOnly:Bool, updateChildren:Bool)
+        {
+                super.__update(transformOnly, updateChildren);
+
+                minutes = '0' + Std.string(curDate.getMinutes());
+                if (curDate.getMinutes() >= 10)
+                {
+                        minutes = Std.string(curDate.getMinutes());
+                }
+                hours = '0' + Std.string(curDate.getHours());
+                if (curDate.getHours() >= 10)
+                {
+                        hours = Std.string(curDate.getHours());
+                }
+                var time:String = curDate.getHours() <= 11 ? 'AM' : 'PM';
+                text = 'Time: ' + hours + ':' + minutes + ' ' + time + '\nDate: ' + calendar.getMonth() + ' ' + curDate.getDate() + ', ' + curDate.getFullYear() 
+                        + '\nDay: ' + calendar.getDay();
+        }
 }
